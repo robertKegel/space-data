0dad866751a42bb60a4f2793375c1df88a7f0db2
robertKegel <kegel.robert@gmail.com>
2021-08-29 20:13:46 -0400

    Fix Developer BASE_URL assignment

--- src/util/LaunchLibrary.js
+++ src/util/LaunchLibrary.js
@@ -1,10 +1,13 @@
-const BASE_URL = 'https://ll.thespacedevs.com/2.0.0';
-const BASE_URL_DEV = 'https://lldev.thespacedevs.com/2.2.0';
+// Production URL
+// const BASE_URL = 'https://ll.thespacedevs.com/2.0.0';
+
+// Developer URL
+const BASE_URL = 'https://lldev.thespacedevs.com/2.2.0';
 
 const LaunchLibrary = {
   getAgencyList(searchTerm=null) {
     // route /agencies/
-    let route = BASE_URL_DEV + '/agencies/';
+    let route = BASE_URL + '/agencies/';
     if (searchTerm) {
       route += '?search=' + searchTerm;
     }
@@ -12,7 +15,7 @@ const LaunchLibrary = {
       .then(response => response.json())
   },
   getAgency(id) {
-    let route = BASE_URL_DEV + '/agencies/' + id;
+    let route = BASE_URL + '/agencies/' + id;
     return fetch(route)
       .then(response => response.json())
   },
