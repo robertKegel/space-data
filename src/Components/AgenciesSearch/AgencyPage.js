2df7c99e0f983048d2aea2f6d98ea60d7212e5fe
robertKegel <kegel.robert@gmail.com>
2021-09-11 00:47:09 -0400

    Add Accordion to launchers and spacecraft

--- src/Components/AgenciesSearch/AgencyPage.js
+++ src/Components/AgenciesSearch/AgencyPage.js
@@ -1,5 +1,5 @@
-import { Paper, Typography, Button, List, ListItem, ListItemText, ListItemAvatar, Avatar, Grid, Divider } from '@material-ui/core';
-
+import { Paper, Typography, Button, List, ListItem, ListItemText, ListItemAvatar, Avatar, Grid, Divider, Accordion, AccordionSummary, AccordionDetails } from '@material-ui/core';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 export default function AgencyPage(props) {
 
   if (!props.agency.image_url) {
@@ -26,14 +26,22 @@ export default function AgencyPage(props) {
           <Grid item xs={6}>
           <a href={props.agency.wiki_url}>
             <div className='agencypage-links'>
-              <img src='https://upload.wikimedia.org/wikipedia/en/thumb/8/80/Wikipedia-logo-v2.svg/800px-Wikipedia-logo-v2.svg.png' width='96px' height='96px' alt='Wikipedia'></img>
+              <img src='https://upload.wikimedia.org/wikipedia/en/thumb/8/80/Wikipedia-logo-v2.svg/800px-Wikipedia-logo-v2.svg.png' 
+                width='96px' 
+                height='96px' 
+                alt='Wikipedia'
+              />  
             </div>
           </a>
           </Grid>
         </Grid>
         <Divider />
-        <Typography variant='body2' gutterBottom className='agencypage-indent'>{props.agency.country_code.length > 3 ? 'Countries:' : 'Country:'} {props.agency.country_code}</Typography>
-        <Typography variant='body1' gutterBottom className='agencypage-indent'>{props.agency.description}</Typography>
+        <Typography variant='body2' gutterBottom className='agencypage-indent'>
+          {props.agency.country_code.length > 3 ? 'Countries:' : 'Country:'} {props.agency.country_code}
+        </Typography>
+        <Typography variant='body1' gutterBottom className='agencypage-indent'>
+          {props.agency.description}
+        </Typography>
       </Paper>
 
       <Grid container spacing={3} alignContent='space-around'>
@@ -66,27 +74,33 @@ export default function AgencyPage(props) {
           <Paper>
             <img src={props.agency.nation_url} width='100%' alt={props.agency.country_code}></img>
             <Divider />
-            <Typography variant='h4' align='center'>Launchers</Typography>
-            <List>
-            {props.agency.launcher_list.map((launcher, index, array) => {
-              let isLastLauncher = index === array.length - 1;
-              return (
-                <div>
-                <ListItem id={launcher.id.toString()}>
-                  <ListItemText 
-                    primary={launcher.name}
-                    secondary={launcher.description}
-                  />
-                  <ListItemAvatar>
-                  {launcher.image_url ? <Avatar src={launcher.image_url} alt={launcher.name} variant='rounded' /> : ''}
-                  </ListItemAvatar>
-                </ListItem>
-                { isLastLauncher ? '' : <Divider /> }
-                </div>
-                
-              )
-            })}
-            </List>
+            <Accordion>
+              <AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls='launcher-content' id='launcher-header'>
+                <Typography variant='h4' align='center'>Launchers</Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <List>
+                {props.agency.launcher_list.map((launcher, index, array) => {
+                  let isLastLauncher = index === array.length - 1;
+                  return (
+                    <div>
+                    <ListItem id={launcher.id.toString()}>
+                      <ListItemText 
+                        primary={launcher.name}
+                        secondary={launcher.description}
+                      />
+                      <ListItemAvatar>
+                      {launcher.image_url ? <Avatar src={launcher.image_url} alt={launcher.name} variant='rounded' /> : ''}
+                      </ListItemAvatar>
+                    </ListItem>
+                    { isLastLauncher ? '' : <Divider /> }
+                    </div>
+                    
+                  )
+                })}
+                </List>
+              </AccordionDetails>
+            </Accordion>
           </Paper>
         </Grid>
 
@@ -94,26 +108,32 @@ export default function AgencyPage(props) {
             <Paper>
               <img src={props.agency.logo_url} width='100%' alt={props.agency.name}></img>
               <Divider />
-              <Typography variant='h4' align='center'>Spacecraft</Typography>
-              <List>
-                {props.agency.spacecraft_list.map((spacecraft, index, array) => {
-                  let isLastSpacecraft = index === array.length - 1;
-                  return (
-                    <div>
-                      <ListItem id={spacecraft.id.toString()}>
-                        <ListItemText 
-                          primary={spacecraft.name}
-                          secondary={spacecraft.history}
-                        />
-                        <ListItemAvatar>
-                        {spacecraft.image_url ? <Avatar src={spacecraft.image_url} alt={spacecraft.name} variant='rounded' /> : ''}
-                        </ListItemAvatar>
-                      </ListItem>
-                      { isLastSpacecraft ? '' : <Divider />}
-                    </div>
-                  )
-                })}
-              </List>
+              <Accordion>
+                <AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls='spacecraft-content' id='spacecraft-header'>
+                  <Typography variant='h4' align='center'>Spacecraft</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                  <List>
+                    {props.agency.spacecraft_list.map((spacecraft, index, array) => {
+                      let isLastSpacecraft = index === array.length - 1;
+                      return (
+                        <div>
+                          <ListItem id={spacecraft.id.toString()}>
+                            <ListItemText 
+                              primary={spacecraft.name}
+                              secondary={spacecraft.history}
+                            />
+                            <ListItemAvatar>
+                            {spacecraft.image_url ? <Avatar src={spacecraft.image_url} alt={spacecraft.name} variant='rounded' /> : ''}
+                            </ListItemAvatar>
+                          </ListItem>
+                          { isLastSpacecraft ? '' : <Divider />}
+                        </div>
+                      )
+                    })}
+                  </List>
+                </AccordionDetails>
+              </Accordion>
             </Paper>
         </Grid>
 
