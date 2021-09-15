d36817d4268d707981c89e6c8bce9040d9b8419d
robertKegel <kegel.robert@gmail.com>
2021-09-15 07:13:09 -0400

    Add SearchSelector Component and state to App

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,5 +1,7 @@
-import AgenciesSearch from "../AgenciesSearch/AgenciesSearch";
 import Events from "../Events/Events";
+import SearchSelector from '../SearchSelector/SearchSelector.js';
+import Agencies from "../Agencies/Agencies";
+import Astronaut from '../Astronaut/Astronaut';
 import { Grid, Hidden } from "@material-ui/core";
 import React from 'react';
 import './App.css';
@@ -7,17 +9,31 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 
 function App() {
 
+  const [page, setPage] = React.useState('agencies');
+  
+  function changeSearch(selectedPage) {
+    setPage(selectedPage)
+  }
+
+  const pages = {
+    'agencies': <Agencies />,
+    'astronaut': <Astronaut />
+  }
+
   return (
     <React.Fragment>
       <CssBaseline />
-      <Grid container justifyContent='space-around'>
-        <Hidden only={['xs', 'sm']}>
-        <Grid item xs={10} md={5}>
-          <Events />
+      <Grid container>
+        <Grid item xs={12}>
+          <SearchSelector changeSearch={changeSearch} pages={pages} page={page}/>
         </Grid>
+        <Hidden only={['xs', 'sm']}>
+          <Grid item md={5}>
+            <Events />
+          </Grid>
         </Hidden>
-        <Grid item xs={10} md={7}>
-          <AgenciesSearch />
+        <Grid item xs={12} md={7}>
+          {pages[page]}
         </Grid>
       </Grid>
     </React.Fragment>
---
+++ src/Components/SearchSelector/SearchSelector.js
@@ -0,0 +1,21 @@
+import { Button, ButtonGroup } from '@material-ui/core';
+
+export default function SearchSelector(props) {
+
+
+  let availablePages = [];
+  if (props.pages) {
+    availablePages = Object.keys(props.pages);
+  }
+
+  return (
+    <ButtonGroup className='button-group' variant="contained" color="primary" aria-label="contained primary button group">
+      {availablePages.map((page) => {
+        let isDisabled = page === props.page;
+        return (
+          <Button disabled={isDisabled} onClick={ () => { props.changeSearch(page) }} >{page.toUpperCase()}</Button>
+        )
+      })}
+    </ButtonGroup>
+  )
+}
